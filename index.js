53fe3a5b1620a0e697714335f9417167f8e6e733
Karim3214s <shaik.karim3214@gmail.com>
2024-07-12 11:27:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure this path matches the location of App.js
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
